0dcd57ed51980397e6d12cc1bb64ec41701bb226
Josh <jkrymgold@gmail.com>
2024-09-28 21:03:10 -0400

    save

--- breadboard/getOut.py
+++ breadboard/getOut.py
@@ -1,27 +1,27 @@
 import requests
 
 if __name__=='__main__':
-    url = r"https://breadboard-community.wl.r.appspot.com/boards/@AdorableFox/quest.bgl.api/run"
+    url = r"http://breadboard-community.wl.r.appspot.com/boards/@AdorableFox/quest.bgl.json"
     headers = {
       "Content-Type": "application/json",
     }
-    params = {
-              "$key": "bb-1t5q3bb3g5w6to47lv3j5k1y5c515g265i6b3o3k292g4t303h",
-              "context": [
-                {
-                  "role": "user",
-                  "parts": [
-                    {
-                      "text": "How big is the moon?"
-                    }
-                  ]
-                }
-              ]
-            }
     #params = {
-    #        
-    #          "$key": "bb-1d34543t634r5lln245o6g1n1h1w5j635t5ega1n1o64115x49"
-    #          }
+    #          "$key": "bb-1t5q3bb3g5w6to47lv3j5k1y5c515g265i6b3o3k292g4t303h",
+    #          "context": [
+    #            {
+    #              "role": "user",
+    #              "parts": [
+    #                {
+
+    #                  "text": "How big is the moon?"
+    #                }
+    #              ]
+    #            }
+    #          ]
+    #        }
+    params = {
+              "key": "rb-1d34543t634r5lln245o6g1n1h1w5j635t5ega1n1o64115x49"
+              }
     print(url)
-    print(requests.post(url).status_code)#,params=params))
+    print(requests.get(url, params=params).status_code)#,params=params))
 

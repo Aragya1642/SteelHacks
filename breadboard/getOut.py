1d6c1d3fdfecb0d437acba6c73161523c865a561
Josh <jkrymgold@gmail.com>
2024-09-28 20:52:25 -0400

    save

--- breadboard/getOut.py
+++ breadboard/getOut.py
@@ -1,8 +1,10 @@
 import requests
 
 if __name__=='__main__':
-    print("Hello World")
-    url = r"http://breadboard-community.wl.r.appspot.com/boards/@AdorableFox/quest.bgl.api/run"
+    url = r"https://breadboard-community.wl.r.appspot.com/boards/@AdorableFox/quest.bgl.api/run"
+    headers = {
+      "Content-Type": "application/json",
+    }
     params = {
               "$key": "bb-1t5q3bb3g5w6to47lv3j5k1y5c515g265i6b3o3k292g4t303h",
               "context": [
@@ -16,6 +18,10 @@ if __name__=='__main__':
                 }
               ]
             }
+    #params = {
+    #        
+    #          "$key": "bb-1d34543t634r5lln245o6g1n1h1w5j635t5ega1n1o64115x49"
+    #          }
     print(url)
-    print(requests.post(url,params=params))
+    print(requests.post(url).status_code)#,params=params))
 
